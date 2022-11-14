addbceb52e1e54fc293368d2bcbde2c13ad6e33b
Nigga <shashkov.danila12@gmail.com>
2022-11-14 12:02:19 +0300

    fixed parse_star/planet_parameters in solar_input.py fixed bug in solar_vis.py

--- solar_input.py
+++ solar_input.py
@@ -19,12 +19,15 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
             else:
-                print("Unknown space object")
+                print('Unknown space object. Available objects are "planet" and "star"')
 
     return objects
 
@@ -43,8 +46,15 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    star, R, color, m, x, y, Vx, Vy = line.split()
+    Star.R = float(R)
+    Star.m = float(m)
+    Star.color = color
+    Star.x = float(x)
+    Star.y = float(y)
+    Star.Vx = float(Vx)
+    Star.Vy = float(Vy)
 
-    pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +71,14 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    planet, R, color, m, x, y, Vx, Vy = line.split()
+    Planet.R = float(R)
+    Planet.m = float(m)
+    Planet.color = tuple(color)
+    Planet.x = float(x)
+    Planet.y = float(y)
+    Planet.Vx = float(Vx)
+    Planet.Vy = float(Vy)
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
