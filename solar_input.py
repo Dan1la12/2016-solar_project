a2a0adef8255943bb91533729a51793b8b25f6ca
Dan1la12 <113887749+Dan1la12@users.noreply.github.com>
2022-11-27 23:59:05 +0300

    fixed saving function, now separate lines, corrected parametrs

--- solar_input.py
+++ solar_input.py
@@ -95,8 +95,9 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            obj_param_line = str(obj.type) + ' ' + str(obj.R)  + ' ' + str(obj.m) + ' ' + str(obj.color) + ' ' + str(obj.x) + ' ' + str(obj.y) + ' ' + str(obj.Vx) + ' ' + str(obj.Vy)
+            obj_param_line = str(obj.type) + ' ' + str(obj.R)  + ' ' + str(obj.color) + ' ' + str(obj.m) + ' ' + str(obj.x) + ' ' + str(obj.y) + ' ' + str(obj.Vx) + ' ' + str(obj.Vy)
             out_file.write(obj_param_line)
+            out_file.write(\n)
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
             out_file.flush()
 
